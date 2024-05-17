200283f30ce466c8db7e511b4ffd8e5770512555
witchdocsec <107813117+witchdocsec@users.noreply.github.com>
2024-05-17 20:59:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.12'
 DESCRIPTION = 'Send malicious pickles via requests or sockets'
 LONG_DESCRIPTION = 'A package that allows the user to easily send out malicious pickles, via web requests, or a malicious server or client(currently ipv4 only)'
 
@@ -20,10 +20,10 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['pickle', 'base64', 'requests', 'socket', 'argparse'],
+    install_requires=['argparse'],
     keywords=['python', 'hack', 'pickle', 'serialization', 'security', 'sockets', 'web'],
     classifiers=[
-        "Development Status :: 1 - Release",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
@@ -31,4 +31,3 @@ setup(
         "Operating System :: Microsoft :: Windows",
     ]
 )
-
