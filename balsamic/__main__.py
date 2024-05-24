2a7920b45414c34263bb99ec2e097b7d9b21c408
witchdocsec <107813117+witchdocsec@users.noreply.github.com>
2024-05-24 23:09:25 +0100

    Update __main__.py

--- balsamic/__main__.py
+++ balsamic/__main__.py
@@ -27,6 +27,7 @@ if __name__ == "__main__":
         socksendparser.add_argument("-c", "--command")
         socksendparser.add_argument("-s", "--steps", default="0")
         socksendparser.add_argument("-e", "--encode", action="store_true")
+        socksendparser.add_argument("--ipv6", action="store_true", help="Use IPv6")
 
         socklistenparser = subparse.add_parser("socklisten")
         socklistenparser.add_argument("-lp", "--lport", required=True)
@@ -34,6 +35,7 @@ if __name__ == "__main__":
         socklistenparser.add_argument("-c", "--command")
         socklistenparser.add_argument("-s", "--steps", default="0")
         socklistenparser.add_argument("-e", "--encode", action="store_true")
+        socklistenparser.add_argument("--ipv6", action="store_true", help="Use IPv6")
         
         # Return parsed arguments
         args = parser.parse_args()
@@ -51,6 +53,6 @@ if __name__ == "__main__":
     if args.attack == "webreq":
         balsamic.webreq(args.method, args.url, args.payload, args.parameter, args.cookie, custom_headers=custom_headers)
     elif args.attack == "socksend":
-        balsamic.socksend(args.rhost, args.rport, args.payload, args.encode, args.steps)
+        balsamic.socksend(args.rhost, args.rport, args.payload, args.encode, args.steps,args.ipv6)
     elif args.attack == "socklisten":
-        balsamic.socklisten(args.lport, args.payload, args.encode, args.steps)
+        balsamic.socklisten(args.lport, args.payload, args.encode, args.steps,args.ipv6)
